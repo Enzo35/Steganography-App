393a7d7d19f9585a8e8905954db00bfa9e8f6701
Enzo Gusmao <enzogusmaobezerra@outlook.com>
2023-09-09 07:03:19 -0300

    stored 2 html  elements and  2 id's in index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("zzzzzz");
+const canvas = document.querySelector("canvas");
+const targetInput = documeny.querySelector("#target");
+const encryptionInput = document.querySelector("#encryption");
+const saveButton = document.querySelector("button");
+let target;
+let encryption;
+
+canvas.width = 300;
+canvas.height = 300;
+
