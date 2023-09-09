707870f97fab1181b75045ae2e03721f2bd4afcc
Enzo Gusmao <enzogusmaobezerra@outlook.com>
2023-09-09 07:14:23 -0300

    added 3 events and 1 function in index.js

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ let encryption;
 canvas.width = 300;
 canvas.height = 300;
 
+targetInput.onchange = (e) => {
+    const img = new SimpleImage(targetInput);
+    img.setSize(300, 300);
+    target = img;
+};
+
+encryptionInput.onchange = (e) => {
+    const img = new SimpleImage(encryptionInput);
+    img.setSize(300, 300);
+    encryption = img;
+}
+
+saveButton.onclick = save;
+
+function getValue(x) {
+    return x - (x % 16);
+}
